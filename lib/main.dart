c2496a1e908c7a8334cb30f2a65919f74789954c
Gareth Beall <gareth.beall@gmail.com>
2023-01-08 20:49:18 +1000

    added appBar

--- lib/main.dart
+++ lib/main.dart
@@ -43,78 +43,140 @@ class _MyHomePageState extends State<MyHomePage> {
     final textTheme = Theme.of(context).textTheme;
     final textWidth = MediaQuery.of(context).size.width * 0.5;
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.background,
+        actions: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.4,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: TextButton(
+                    child: Text(
+                      'HOME',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        fontSize: textTheme.headline6!.fontSize,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    child: Text(
+                      'ABOUT',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        fontSize: textTheme.headline6!.fontSize,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    
+                    child: Text(
+                      'PROJECTS',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        fontSize: textTheme.headline6!.fontSize,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                const SizedBox(
+                  width: 25,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
       body: Container(
         color: Theme.of(context).colorScheme.background,
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-            SizedBox(
-              width: textWidth,
-              child: Text('Hello.',
-                  textAlign: TextAlign.start,
-                  style: textTheme.headline2!.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  )),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              width: textWidth,
-              child: Text('My name is Gareth Beall.',
-                  textAlign: TextAlign.start,
-                  style: textTheme.headline4!.copyWith(
-                    color: Theme.of(context).colorScheme.secondary,
-                    fontWeight: FontWeight.bold,
-                  )),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              width: textWidth,
-              child: RichText(
-                  text: TextSpan(children: [
-                TextSpan(
-                  text:
-                      'I\'m a Flutter developer with 2 years of experience creating beautiful and functional apps. \n\nI\'m passionate about building intuitive and engaging user experiences, and I love the flexibility and power that Flutter gives me to do that. \n\nMost of my work is open source and publicly available on  ',
-                  style: textTheme.headline5!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+        child: ListView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
                 ),
-                TextSpan(
-                  text: 'GitHub.',
-                  style: textTheme.headline5!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                    decoration: TextDecoration.underline,
-                  ),
+                SizedBox(
+                  width: textWidth,
+                  child: Text('Hello.',
+                      textAlign: TextAlign.start,
+                      style: textTheme.headline2!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                      )),
                 ),
-                TextSpan(
-                  text: 'You can follow me on  ',
-                  style: textTheme.headline5!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+                const SizedBox(
+                  height: 25,
                 ),
-                TextSpan(
-                  text: 'Twitter ',
-                  style: textTheme.headline5!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+                SizedBox(
+                  width: textWidth,
+                  child: Text('My name is Gareth Beall.',
+                      textAlign: TextAlign.start,
+                      style: textTheme.headline4!.copyWith(
+                        color: Theme.of(context).colorScheme.secondary,
+                        fontWeight: FontWeight.bold,
+                      )),
                 ),
-                TextSpan(
-                  text:
-                      'where I mostly tweet about Vue and frontend technologies.\n\n When I\'m not coding, you can find me surfing, cooking up a storm in the kitchen, or spending quality time with my friends and family. \n\nThank you for stopping by my portfolio, and I hope you enjoy what you see!',
-                  style: textTheme.headline5!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+                const SizedBox(
+                  height: 25,
                 ),
-              ])),
+                SizedBox(
+                  width: textWidth,
+                  child: RichText(
+                      text: TextSpan(children: [
+                    TextSpan(
+                      text:
+                          'I\'m a Flutter developer with 2 years of experience creating beautiful and functional apps. \n\nI\'m passionate about building intuitive and engaging user experiences, and I love the flexibility and power that Flutter gives me to do that. \n\nMost of my work is open source and publicly available on  ',
+                      style: textTheme.headline5!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'GitHub.',
+                      style: textTheme.headline5!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'You can follow me on  ',
+                      style: textTheme.headline5!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Twitter ',
+                      style: textTheme.headline5!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                    ),
+                    TextSpan(
+                      text:
+                          'where I mostly tweet about Vue and frontend technologies.\n\n When I\'m not coding, you can find me surfing, cooking up a storm in the kitchen, or spending quality time with my friends and family. \n\nThank you for stopping by my portfolio, and I hope you enjoy what you see!',
+                      style: textTheme.headline5!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                    ),
+                  ])),
+                ),
+              ],
             ),
           ],
         ),
